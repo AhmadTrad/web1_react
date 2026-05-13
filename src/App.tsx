43edcafb6ea36ec9b086273b0a8552ae36b8fbc8
ahmadtrad <ahmad.trad@hotmail.com>
2026-05-13 09:18:55 +0300

    add list

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import MyButton from "./components/my-button/my-button";
 
 const App = () => {
+  const addCourse = () => {};
+  let courses = ["CSC101", "CSC102", "CSC103", "CSC104", "CSC105", "CSC106"];
   return (
     <>
       <MyButton text="Butn 10" isHidden={false} />
       <MyButton isHidden={true} />
+      <ul>
+        {courses.map((course, index) => {
+          return <li key={index}>{course}</li>;
+        })}
+      </ul>
+      <input type="text" />
+      <button onClick={addCourse}>Add Course</button>
     </>
   );
 };
